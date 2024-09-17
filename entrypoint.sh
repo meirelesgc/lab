afbe77e930271f6ad1df79559203ddc494806e86
meirelesgc <geu_costa@outlook.com>
2024-09-17 20:24:19 -0300

    ajuste no entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 poetry run uvicorn lab:app --host 0.0.0.0 --port 8000 --reload &
 
-poetry run celery -A lab.celery worker --loglevel=INFO --concurrency=1 &
+poetry celery -A lab.routers.documents.celery worker --loglevel=INFO --concurrency=1 &
 
 poetry run celery -A lab.celery flower &
 
