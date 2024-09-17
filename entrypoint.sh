17c0b0c64d6447a7b6b621abcb3fa2f1d92710f2
meirelesgc <geu_costa@outlook.com>
2024-09-17 20:56:40 -0300

    corrigindo entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 poetry run uvicorn lab:app --host 0.0.0.0 --port 8000 --reload &
 
-poetry celery -A lab.routers.documents.celery worker --loglevel=INFO --concurrency=1 &
+poetry run celery -A lab.routers.documents.celery worker --loglevel=INFO --concurrency=2 --uid=nobody&
 
-poetry run celery -A lab.celery flower &
+poetry run celery -A lab.routers.documents.celery flower --uid=nobody &
 
 wait -n
