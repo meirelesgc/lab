e1ec8dc801c134746b221a64bb38a396a15e4437
meirelesgc <geu_costa@outlook.com>
2024-10-14 18:24:56 -0300

    ajuste dos cors

--- lab/__init__.py
+++ lab/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic_settings import BaseSettings
 
 
@@ -21,9 +22,23 @@ class Settings(BaseSettings):
 settings = Settings()
 os.environ.update(settings.model_dump())
 
+
 from lab.routers import documents, openAI, parameters, patient  # noqa: E402
 
 app = FastAPI()
+
+origins = [
+    'http://localhost',
+    'http://localhost:5173',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 app.include_router(openAI.router)
 app.include_router(documents.router)
